ecf320d03a73453e1298631bb73b12fd67ecd1d5
pariharsourabh <117932572+pariharsourabh@users.noreply.github.com>
2023-02-27 23:34:19 +0530

    Create str length function.py

---
+++ str length function.py
@@ -0,0 +1,9 @@
+def leng(a):
+    """This is a program which will help to find a lenght of string."""
+    count = 0
+    for i in a:
+        if type(a) == str :
+            count = count + 1
+    return count
+
+leng("ajkfhakfhka")
